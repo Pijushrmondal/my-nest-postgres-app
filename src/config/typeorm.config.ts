81c0f87eb13c6f1ec8e5249b91b9f0d88a14a123
Pijush Mondal <pijush@antino.com>
2025-09-29 12:59:14 +0530

    database config setup

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,7 +8,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'blive',
-  migrationsRun: false,
   synchronize: false,
+  entities: ['dist/database/entity/*.entity.js'],
   logging: true,
 };
